e91cd45c54c09e37620dd8a2cf0f97757c278121
Konstantin Gründger <kammermark@gmx.de>
2019-09-07 20:16:02 +0200

    Renamed database file

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or "sqlite:///" + os.path.join(basedir, "data.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LANGUAGES = ["en", "de"]
