525a4c822eeb9fb12e873c1609b97e3c31c004a7
Konstantin Gründger <kammermark@gmx.de>
2020-04-04 14:54:00 +0200

    Implement and test legacy download routes

--- app/main/legacy_routes.py
+++ app/main/legacy_routes.py
@@ -1,6 +1,6 @@
 from io import StringIO
 import csv
-from flask import request, make_response
+from flask import request, make_response, jsonify
 from app import db
 from app.models import Device
 from app.main import bp
@@ -9,6 +9,7 @@ from app.main import bp
 @bp.route("/download")
 def download():
     t = request.args.get("t")
+    j = request.args.get("j")
     device_id = request.args.get("device_id")
     from_id = request.args.get("from_id")
     till_id = request.args.get("till_id")
@@ -29,47 +30,67 @@ def download():
     elif cn:
         filter_args = [Device.cn.in_(cn.split(","))]
 
-    output = StringIO()
+    devices = db.session.query(Device).filter(*filter_args).order_by(Device.address)
+    if j:
+        output = []
+        for device in devices:
+            show = device.show_track and device.show_identity
+            data = {"device_type": device.device_type.name[0],
+                    "device_id": device.address,
+                    "aircraft_model": device.aircraft_type.name if show else "",
+                    "registration": device.registration if show else "",
+                    "cn": device.cn if show else "",
+                    "tracked": "Y" if device.show_track else "N",
+                    "identified": "Y" if device.show_identity else "N"
+                    }
+            if t:
+                data["aircraft_type"] = str(device.aircraft_type.category.value)
 
-    header_writer = csv.writer(output)
-    csv_header = ["#DEVICE_TYPE", "DEVICE_ID", "AIRCRAFT_MODEL", "REGISTRATION", "CN", "TRACKED", "IDENTIFIED", "AIRCRAFT_TYPE" "AIRCRAFT_TYPE"]
-    header_writer.writerow(csv_header if t else csv_header[:-1])
+            output.append(data)
 
-    row_writer = csv.writer(output, quotechar="'", quoting=csv.QUOTE_ALL)
-    for device in db.session.query(Device).filter(*filter_args):
-        if device.show_track and device.show_identity:
-            csv_data = [
-                device.device_type.name[0],
-                device.address,
-                device.aircraft_type.name,
-                device.registration,
-                device.cn,
-                "Y" if device.show_track else "N",
-                "Y" if device.show_identity else "N",
-                device.aircraft_type.category.value,
-            ]
-        else:
-            csv_data = [
-                device.device_type.name[0],
-                "", "", "", "",
-                "Y" if device.show_track else "N",
-                "Y" if device.show_identity else "N",
-                device.aircraft_type.category.value,
-            ]
-        row_writer.writerow(csv_data if t else csv_data[:-1])
+        return jsonify({"devices": output})
+    else:
+        output = StringIO()
 
-    # return file text/csv
-    '''
-    buffer = BytesIO()
-    buffer.write(output.getvalue().encode("utf-8"))
-    buffer.seek(0)
-    return send_file(buffer, mimetype="text/comma-separated-values", attachment_filename="ddb.csv", as_attachment=True)
-    '''
+        header_writer = csv.writer(output, lineterminator='\n')
+        csv_header = ["#DEVICE_TYPE", "DEVICE_ID", "AIRCRAFT_MODEL", "REGISTRATION", "CN", "TRACKED", "IDENTIFIED", "AIRCRAFT_TYPE"]
+        header_writer.writerow(csv_header if t else csv_header[:-1])
 
-    # return text/plain
-    response = make_response(output.getvalue().encode("utf-8"), 200)
-    response.mimetype = "text/plain"
-    return response
+        row_writer = csv.writer(output, quotechar="'", quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for device in devices:
+            if device.show_track and device.show_identity:
+                csv_data = [
+                    device.device_type.name[0],
+                    device.address,
+                    device.aircraft_type.name,
+                    device.registration,
+                    device.cn,
+                    "Y" if device.show_track else "N",
+                    "Y" if device.show_identity else "N",
+                    device.aircraft_type.category.value,
+                ]
+            else:
+                csv_data = [
+                    device.device_type.name[0],
+                    "", "", "", "",
+                    "Y" if device.show_track else "N",
+                    "Y" if device.show_identity else "N",
+                    device.aircraft_type.category.value,
+                ]
+            row_writer.writerow(csv_data if t else csv_data[:-1])
+
+        # return file text/csv
+        '''
+        buffer = BytesIO()
+        buffer.write(output.getvalue().encode("utf-8"))
+        buffer.seek(0)
+        return send_file(buffer, mimetype="text/comma-separated-values", attachment_filename="ddb.csv", as_attachment=True)
+        '''
+
+        # return text/plain
+        response = make_response(output.getvalue().encode("utf-8"), 200)
+        response.mimetype = "text/plain"
+        return response
 
 
 @bp.route("/download-fln.php")
---
+++ tests/test_legacy.py
@@ -0,0 +1,156 @@
+import unittest
+import json
+from app.filldata import import_fakedata
+from .base import TestCaseBase
+
+
+class TestLegacy(TestCaseBase):
+    def test_download(self):
+        import_fakedata()
+        with self.app.test_client() as c:
+            response = c.get(f'/download', follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.charset, 'utf-8')
+            self.assertEqual(response.mimetype, 'text/plain')
+            data = response.get_data().decode('utf-8')
+
+            shouldbe = (
+                "#DEVICE_TYPE,DEVICE_ID,AIRCRAFT_MODEL,REGISTRATION,CN,TRACKED,IDENTIFIED\n"
+                "'O','ABCDEF','Rhönlärche Ил-32','D-OTTO','TO','Y','Y'\n"
+                "'F','DD1234','ASK-13','D-1234','34','Y','Y'\n"
+                "'F','DD4711','ASH-25','D-4711','11','Y','Y'\n"
+            )
+
+            self.assertEqual(data, shouldbe)
+
+    def test_download_t(self):
+        import_fakedata()
+        with self.app.test_client() as c:
+            response = c.get(f'/download?t=1', follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.charset, 'utf-8')
+            self.assertEqual(response.mimetype, 'text/plain')
+            data = response.get_data().decode('utf-8')
+
+            shouldbe = (
+                "#DEVICE_TYPE,DEVICE_ID,AIRCRAFT_MODEL,REGISTRATION,CN,TRACKED,IDENTIFIED,AIRCRAFT_TYPE\n"
+                "'O','ABCDEF','Rhönlärche Ил-32','D-OTTO','TO','Y','Y','1'\n"
+                "'F','DD1234','ASK-13','D-1234','34','Y','Y','1'\n"
+                "'F','DD4711','ASH-25','D-4711','11','Y','Y','1'\n"
+            )
+
+            self.assertEqual(data, shouldbe)
+
+    def test_download_j(self):
+        import_fakedata()
+        with self.app.test_client() as c:
+            response = c.get(f'/download?j=1', follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.charset, 'utf-8')
+            self.assertEqual(response.mimetype, 'application/json')
+            self.assertTrue(response.is_json)
+            data = response.get_data()
+            json_data = json.loads(data)
+
+            shouldbe = {
+                "devices": [
+                    {"aircraft_model": "Rhönlärche Ил-32", "cn": "TO", "device_id": "ABCDEF", "device_type": "O", "identified": "Y", "registration": "D-OTTO", "tracked": "Y"},
+                    {"aircraft_model": "ASK-13", "cn": "34", "device_id": "DD1234", "device_type": "F", "identified": "Y", "registration": "D-1234", "tracked": "Y"},
+                    {"aircraft_model": "ASH-25", "cn": "11", "device_id": "DD4711", "device_type": "F", "identified": "Y", "registration": "D-4711", "tracked": "Y"},
+                ]}
+
+            self.assertEqual(json_data, shouldbe)
+
+    def test_download_j_t(self):
+        import_fakedata()
+        with self.app.test_client() as c:
+            response = c.get(f'/download?j=1&t=1', follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.charset, 'utf-8')
+            self.assertEqual(response.mimetype, 'application/json')
+            self.assertTrue(response.is_json)
+            data = response.get_data()
+            json_data = json.loads(data)
+
+            shouldbe = {
+                "devices": [
+                    {"aircraft_model": "Rhönlärche Ил-32", "cn": "TO", "device_id": "ABCDEF", "device_type": "O", "identified": "Y", "registration": "D-OTTO", "tracked": "Y", "aircraft_type": "1"},
+                    {"aircraft_model": "ASK-13", "cn": "34", "device_id": "DD1234", "device_type": "F", "identified": "Y", "registration": "D-1234", "tracked": "Y", "aircraft_type": "1"},
+                    {"aircraft_model": "ASH-25", "cn": "11", "device_id": "DD4711", "device_type": "F", "identified": "Y", "registration": "D-4711", "tracked": "Y", "aircraft_type": "1"},
+                ]}
+
+            self.assertEqual(json_data, shouldbe)
+
+    def test_download_device_id(self):
+        import_fakedata()
+        with self.app.test_client() as c:
+            response = c.get(f'/download?device_id=DD1234,ABCDEF', follow_redirects=True)
+            data = response.get_data().decode('utf-8')
+
+            shouldbe = (
+                "#DEVICE_TYPE,DEVICE_ID,AIRCRAFT_MODEL,REGISTRATION,CN,TRACKED,IDENTIFIED\n"
+                "'O','ABCDEF','Rhönlärche Ил-32','D-OTTO','TO','Y','Y'\n"
+                "'F','DD1234','ASK-13','D-1234','34','Y','Y'\n"
+            )
+
+            self.assertEqual(data, shouldbe)
+
+    def test_download_from_id(self):
+        import_fakedata()
+        with self.app.test_client() as c:
+            response = c.get(f'/download?from_id=DD1234', follow_redirects=True)
+            data = response.get_data().decode('utf-8')
+
+            shouldbe = (
+                "#DEVICE_TYPE,DEVICE_ID,AIRCRAFT_MODEL,REGISTRATION,CN,TRACKED,IDENTIFIED\n"
+                "'F','DD1234','ASK-13','D-1234','34','Y','Y'\n"
+                "'F','DD4711','ASH-25','D-4711','11','Y','Y'\n"
+            )
+
+            self.assertEqual(data, shouldbe)
+
+    def test_download_till_id(self):
+        import_fakedata()
+        with self.app.test_client() as c:
+            response = c.get(f'/download?till_id=DD1234', follow_redirects=True)
+            data = response.get_data().decode('utf-8')
+
+            shouldbe = (
+                "#DEVICE_TYPE,DEVICE_ID,AIRCRAFT_MODEL,REGISTRATION,CN,TRACKED,IDENTIFIED\n"
+                "'O','ABCDEF','Rhönlärche Ил-32','D-OTTO','TO','Y','Y'\n"
+                "'F','DD1234','ASK-13','D-1234','34','Y','Y'\n"
+            )
+
+            self.assertEqual(data, shouldbe)
+
+    def test_download_registration(self):
+        import_fakedata()
+        with self.app.test_client() as c:
+            response = c.get(f'/download?registration=D-4711,D-OTTO', follow_redirects=True)
+            data = response.get_data().decode('utf-8')
+
+            shouldbe = (
+                "#DEVICE_TYPE,DEVICE_ID,AIRCRAFT_MODEL,REGISTRATION,CN,TRACKED,IDENTIFIED\n"
+                "'O','ABCDEF','Rhönlärche Ил-32','D-OTTO','TO','Y','Y'\n"
+                "'F','DD4711','ASH-25','D-4711','11','Y','Y'\n"
+            )
+
+            self.assertEqual(data, shouldbe)
+
+    def test_download_cn(self):
+        import_fakedata()
+        with self.app.test_client() as c:
+            response = c.get(f'/download?cn=34,TO', follow_redirects=True)
+            data = response.get_data().decode('utf-8')
+
+            shouldbe = (
+                "#DEVICE_TYPE,DEVICE_ID,AIRCRAFT_MODEL,REGISTRATION,CN,TRACKED,IDENTIFIED\n"
+                "'O','ABCDEF','Rhönlärche Ил-32','D-OTTO','TO','Y','Y'\n"
+                "'F','DD1234','ASK-13','D-1234','34','Y','Y'\n"
+            )
+
+            self.assertEqual(data, shouldbe)
+
+
+if __name__ == "__main__":
+    unittest.main()
